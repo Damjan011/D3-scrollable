248debd5e5086193375d32e45aa0dcd6fdabfe1f
Damjan Piperski <damjanpiperski@gmail.com>
2021-02-16 20:04:56 +0100

    Scrolling and drag and drop working together, starting tooltip

--- src/components/BarChart/index.js
+++ src/components/BarChart/index.js
@@ -1,8 +1,9 @@
 import * as d3 from 'd3';
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import './style.css';
 
 const BarChart = ({ width, height, data }) => {
+  const [delta, setDelta] = useState({ value: 0 });
   const ref = useRef();
 
   useEffect(() => {
@@ -56,56 +57,55 @@ const BarChart = ({ width, height, data }) => {
     const svg = d3.select(ref.current)
       .attr("viewBox", [0, 0, width, height])
       .attr('id', 'bakalar')
+      .attr("cursor", "grab");
 
-      const nestedSvg = svg.append("svg")
+    const nestedSvg = svg.append("svg")
       .attr("viewBox", [width, height])
       .attr("id", "svg-inner")
       .attr("width", 960)
-      .attr('class', 'svg-inner');
+      .attr('class', 'svg-inner')
 
-      const tooltip = d3.select("body").append("div")
+    const tooltip = d3.select("body").append("div")
       .attr("class", "svg-tooltip")
       .style("position", "absolute")
       .style("visibility", "hidden")
       .text("bugraaa");
 
-      d3.select('#bakalar')
-      .on("mouseover", function(){
-        return tooltip.style("visibility", "visible");
-      })
-      .on("mousemove", function(event){
-        let calcPageY = event.pageY;
-        console.log(calcPageY)
-        let calcPageX = event.pageX;
-        console.log(calcPageX)
-        if(calcPageX < 200) {
-          return tooltip.style('visibility', 'hidden')
-        } else {
-        return tooltip.style("top", (calcPageY-10)+"px")
-                      .style("left",(calcPageX+10)+"px")
-                      .style("visibility", "visible");
-        }
-      })
-      .on("mouseout", function(){
-        return tooltip.style("visibility", "hidden");
-      });
+    // d3.select('#bakalar')
+    // .on("mouseover", function(){
+    //   return tooltip.style("visibility", "visible");
+    // })
+    // .on("mousemove", function(event){
+    //   let calcPageY = event.pageY;
+    //   let calcPageX = event.pageX;
+    //   if(calcPageX < 200) {
+    //     return tooltip.style('visibility', 'hidden')
+    //   } else {
+    //   return tooltip.style("top", (calcPageY-10)+"px")
+    //                 .style("left",(calcPageX+10)+"px")
+    //                 .style("visibility", "visible");
+    //   }
+    // })
+    // .on("mouseout", function(){
+    //   return tooltip.style("visibility", "hidden")
+    // });
 
-      nestedSvg.append('g')
+    nestedSvg.append('g')
       .call(xAxis)
       .attr('class', 'x-axis')
 
-      nestedSvg.append('g')
+    nestedSvg.append('g')
       .call(xGridlines)
       .attr('transform', `translate(0, ${height - 30})`)
       .attr('stroke-dasharray', '5 5')
       .attr('class', 'x-axis-grid')
       .attr('id', 'coban')
 
-    nestedSvg.append("g")			
+    nestedSvg.append("g")
       .call(yGridlines)
       .attr('class', 'y-axis-grid');
 
-      nestedSvg.append("path")
+    nestedSvg.append("path")
       .datum(data)
       .attr("fill", "none")
       .attr("stroke", "#6EE294")
@@ -124,7 +124,7 @@ const BarChart = ({ width, height, data }) => {
       .attr("stroke-linecap", "round")
       .attr("d", line2);
 
-      svg.append('g')
+    svg.append('g')
       .call(yAxis);
 
     const zoom = d3.zoom()
@@ -135,26 +135,88 @@ const BarChart = ({ width, height, data }) => {
         [width * 2, 0]
       ])
       .on('zoom', function (event) {
-        const burgija = event.transform
+        const burgija = event.transform;
+        const patka = event.sourceEvent;
+        console.log(burgija)
+        burgija.k = 1;
+        tooltip.style("visibility", "hidden")
+        if (patka.deltaY) {
+          if (patka.deltaY === 100 && delta.value < 10) {
+            setDelta(delta.value += 50)
+            burgija.x = delta.value
+          } else if (patka.deltaY === -100 && delta.value > -1000) {
+            setDelta(delta.value -= 50)
+            burgija.x = delta.value
+          }
+        } else {
+          setDelta(delta.value = burgija.x)
+        }
         d3.select(this)
           .selectAll('path')
-          .attr("transform", burgija)
-        console.log(d3.select(this))
+          .attr("transform", `translate(${delta.value}, ${0})`)
         d3.select(this)
           .select('g:first-of-type')
-          .attr("transform", `translate(${burgija.x}, ${370})`);
-
+          .attr("transform", `translate(${delta.value}, ${370})`);
         d3.select(this)
           .select('#coban')
-          .attr("transform", `translate(${burgija.x}, ${370})`)
+          .attr("transform", `translate(${delta.value}, ${370})`)
       });
-      svg.call(zoom);
+    svg.call(zoom);
+
+    //     const bisect = d3.bisector(d => d.timestamp).left;
+
+
+    //     const callout = (g, rx) => {
+    //       if (!rx) return g.style("display", "none");
+
+    //       g
+    //       .attr('id', 'newg')
+    //           .style("display", null)
+    //           .style("pointer-events", "none")
+    //           .style("font", "10px sans-serif");
+
+    //       const path = g.selectAll("path")
+    //         .data([null])
+    //         .join("path")
+    //           .attr("fill", "white")
+    //           .attr("stroke", "black");
+
+    //       const text = g.selectAll("text")
+    //         .data([null])
+    //         .join("text")
+    //         .call(text => text
+    //           .selectAll("tspan")
+    //           .data((rx + "").split(/\n/))
+    //           .join("tspan")
+    //             .attr("x", 0)
+    //             .attr("y", (d, i) => `${i * 1.1}em`)
+    //             .style("font-weight", (_, i) => i ? null : "bold")
+    //             .text(d => d));
+
+    //       const {x, y, width: w, height: h} = text.node().getBBox();
+
+    //       text.attr("transform", `translate(${-w / 2},${15 - y})`);
+    //       path.attr("d", `M${-w / 2 - 10},5H-5l5,-5l5,5H${w / 2 + 10}v${h + 20}h-${w + 20}z`);
+    //     }
+
+    // /////////////////////////////////
+    //     const tooltip2 = nestedSvg.append("g");
+
+    //     nestedSvg.on("touchmove mousemove", function(event) {
+    //       const {timestamp, rx} = bisect(d3.pointer(event, this)[0]);
+    //       console.log(bisect(d3.pointer(event, this)[0]))
+    //       tooltip2
+    //           .attr("transform", `translate(${x(timestamp)},${y(rx)})`)
+    //           .call(callout, `${rx} ${timestamp}`);
+    //     });
+
+    //     nestedSvg.on("touchend mouseleave", () => tooltip2.call(callout, null));
 
   }
 
   return (
     <div className="chart">
-      <svg style={{padding: '10px', position: 'relative'}} ref={ref} />
+      <svg style={{ padding: '10px', position: 'relative' }} ref={ref} />
     </div>
   )
 }
